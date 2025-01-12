924eef4f34faaf7991c118503b0f557d82e9e335
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-01-12 12:14:25 +0300

    a

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,16 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
+const connectionString = "postgresql://postgres:jJAcToDFqQOIMyjAkTbFRQzyGyTTXIyW@junction.proxy.rlwy.net:37388/railway"
+
+// module.exports = new Pool({
+//     host:process.env.HOST,
+//     user:process.env.USER,
+//     database:process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: process.env.DATABASE_PORT
+// })
+
 module.exports = new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DATABASE_PORT
-})
+    connectionString,
+});
