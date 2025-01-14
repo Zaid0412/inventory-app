c9595c8f8943c55b99593f21f3187f34f711c517
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-01-14 15:48:16 +0300

    Remove unnecessary console.logs

--- db/pool.js
+++ db/pool.js
@@ -3,14 +3,14 @@ require('dotenv').config()
 
 const connectionString = "postgresql://postgres:jJAcToDFqQOIMyjAkTbFRQzyGyTTXIyW@junction.proxy.rlwy.net:37388/railway"
 
-module.exports = new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DATABASE_PORT
-})
-
 // module.exports = new Pool({
-//     connectionString,
-// });
+//     host:process.env.HOST,
+//     user:process.env.USER,
+//     database:process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: process.env.DATABASE_PORT
+// })
+
+module.exports = new Pool({
+    connectionString,
+});
